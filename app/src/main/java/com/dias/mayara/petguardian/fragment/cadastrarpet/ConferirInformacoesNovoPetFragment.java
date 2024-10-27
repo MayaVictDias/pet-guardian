4b7b29b32a3bfcd1c2cc69251611f081c94ed79e
Mayara Victoria Dias <victdias.mayara@icloud.com>
2024-10-27 01:41:09 -0300

    Criação da interface da tela de revisao de informacoes inseridas sobre um pet antes de publica-lo

--- app/src/main/java/com/dias/mayara/petguardian/fragment/cadastrarpet/ConferirInformacoesNovoPetFragment.java
+++ app/src/main/java/com/dias/mayara/petguardian/fragment/cadastrarpet/ConferirInformacoesNovoPetFragment.java
@@ -1,16 +1,23 @@
 package com.dias.mayara.petguardian.fragment.cadastrarpet;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.dias.mayara.petguardian.R;
+import com.dias.mayara.petguardian.helper.FragmentInteractionListener;
 
 public class ConferirInformacoesNovoPetFragment extends Fragment {
+
+    private Button buttonVoltar;
+    private FragmentInteractionListener listener;
 
     public ConferirInformacoesNovoPetFragment() {
         // Required empty public constructor
@@ -26,6 +33,44 @@ public class ConferirInformacoesNovoPetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_conferir_informacoes_novo_pet, container, false);
+        View view = inflater.inflate(R.layout.fragment_conferir_informacoes_novo_pet, container, false);
+
+        inicializarComponentes(view);
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    getActivity().getSupportFragmentManager().popBackStack();
+                } catch (Exception e) {
+                    e.printStackTrace(); // Imprime o erro no log
+                }
+            }
+        });
+
+        return view;
     }
+
+    private void inicializarComponentes(View view) {
+
+        buttonVoltar = view.findViewById(R.id.buttonVoltar);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof FragmentInteractionListener) {
+            listener = (FragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " deve implementar FragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+
+
 }
